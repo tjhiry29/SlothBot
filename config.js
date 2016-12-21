4f1fefe0b15d7d4e43d38a197158ad6f60ea17d1
tjhiry29 <r.tjhie0809@gmail.com>
2016-12-20 21:11:33 -0500

    added some files

---
+++ config.js
@@ -0,0 +1,12 @@
+const token = "MjUwNDIyMDcyOTkwMjM2Njcy.CxvKIw.YtWMKpNyCnn5yAmVXuFwCPcecCc";
+const youtube_api_key = "AIzaSyCu6aMb3rUs-un76l-USc4GfflqyJhkNNY";
+
+exports.getToken = function() {
+    return token;
+}
+
+exports.getYoutubeKey = function() {
+  return youtube_api_key;
+}
+
+module.exports = exports;
